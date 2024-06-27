92cc9ccaa737132008dba85b29d037909b651927
Gustavo Vieira <gvieira@gmail.com>
2024-06-27 13:15:17 -0300

    Create config.py
    Added config example

---
+++ config.py
@@ -0,0 +1,36 @@
+# config.py example
+
+TOKEN = "bot_token_here"
+GUILD = 1234
+ID = 1234
+OWNER_ID = 1234
+DESCRIPTION = "Path of Exile Brasil Bot"
+BOT_ACTIVITY = "Playing PoE"
+EXTENSIONS = ["lab", "pricecheck", "adm", "owner", "poeninja", "voice", "users", "wiki"]
+LOG_CHANNEL = 1234
+LAB_CHANNEL = 1234
+PRICE_CHECK = 1234
+AFK_CHANNEL = 1234
+GENERAL_CHANNEL = 1234
+LAB_TRIALS_MESSAGE = 1234
+INVITE = "https://discord.gg/invite_here"
+FACEBOOK = "https://www.facebook.com/groups/_____"
+
+POESESSID = "123"
+AGENT = "aaa"
+LEAGUE = "Necropolis"
+
+VOTE_THRESHOLD = 0.7
+
+TOUCAN = """\
+░░░░░░░░▄▄▄▀▀▀▄▄███▄░░░░░░░░░░░░░░
+░░░░░▄▀▀░░░░░░░▐░▀██▌░░░░░░░░░░░░░
+░░░▄▀░░░░▄▄███░▌▀▀░▀█░░░░░░░░░░░░░
+░░▄█░░▄▀▀▒▒▒▒▒▄▐░░░░█▌░░░░░░░░░░░░
+░▐█▀▄▀▄▄▄▄▀▀▀▀▌░░░░░▐█▄░░░░░░░░░░░
+░▌▄▄▀▀░░░░░░░░▌░░░░▄███████▄░░░░░░
+░░░░░░░░░░░░░▐░░░░▐███████████▄░░░
+░░░░░le░░░░░░░▐░░░░▐█████████████▄
+░░░░toucan░░░░░░▀▄░░░▐█████████████▄
+░░░░░░has░░░░░░░░▀▄▄███████████████
+░░░░░arrived░░░░░░░░░░░░█▀██████░░░░░"""
